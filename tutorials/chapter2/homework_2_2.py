0117b466540aed5f7d3a0ed28d69dfb67531e876
Fidel Abella <fabella42@gmail.com>
2014-09-18 02:25:20 -0500

    Finished homework 2

---
+++ tutorials/chapter2/homework_2_2.py
@@ -0,0 +1,30 @@
+import pymongo
+import sys
+
+def drop_lowest_homework_grades():
+   connection = pymongo.MongoClient("mongodb://localhost")
+   db = connection.students
+   grades = db.grades
+
+   try:
+      query = {'type':'homework'}
+
+      scores = grades.find(query).sort([('student_id', pymongo.ASCENDING), ('score', pymongo.ASCENDING)])
+      
+      current_user = None
+      for score in scores:
+         if current_user is None:
+	    current_user = score['student_id']
+	    grades.remove(score)
+	 if current_user != score['student_id']:
+	    current_user = score['student_id']
+	    grades.remove(score)
+
+   except:
+      print "Unexpected error:", sys.exc_info()[0]
+      raise
+
+
+if __name__ == '__main__':
+   drop_lowest_homework_grades()
+
